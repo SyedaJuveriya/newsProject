b56370b518ccacd3d8a24949b65f6edc8423866b
SyedaJuveriya <syedajuveriya00@gmail.com>
2025-08-08 16:17:23 +0530

    news portal with api

---
+++ testapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+import requests
+
+def home_view(request):
+    # define news category
+    category = [
+        'technology',
+        'sports',
+        'health',
+        'business',
+        'entertainment',
+        'general',
+    ]
+
+    dict = {'category':category}
+
+    return render(request,'testapp/home.html', context=dict)
+
+
+def news_view(request,category):
+    # API_KEY = '7bfb4e480a4f4b39930853d249c67a8d'
+
+    url = f"https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey=d7070cf6655145ed85744403aa45a6d4"
+
+
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract ariticles list from API response
+
+    articles = data.get('articles',[]) if data.get('status') == 'ok' else []
+
+    dict = {
+        'articles':articles,
+        'category':category
+
+    }
+
+    return render(request, 'testapp/news_list.html', context=dict)
